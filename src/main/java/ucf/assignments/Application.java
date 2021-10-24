be4b2c879a633f932e60b6f63e33d18451d6202d
Berrybunny015 <berrybunny015@gmail.com>
2021-10-24 12:41:30 -0400

    function movement and additional fxml files

--- src/main/java/ucf/assignments/Application.java
+++ src/main/java/ucf/assignments/Application.java
@@ -5,163 +5,70 @@ package ucf.assignments;
  *  Copyright 2021 Sabrina Lopez
  */
 
+//get any necessary imports
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.util.*;
 
 public class Application extends javafx.application.Application {
 
-    public static void main(String[] args) {
-
-        HashMap<String, HashMap<String, ItemDetails>> outerMap = new HashMap<>();
-        HashMap<String, ItemDetails> innerMap = new HashMap<>();
-
-        launch();
-
-        //create if-else statements to track user's choices within the Screen Builder and then use the functions accordingly
-
-        innerMap = addItem(innerMap);
-        outerMap = addToDoList(outerMap, innerMap);
-
-        innerMap = editItemDueDate(innerMap);
-        innerMap = editItemDescription(innerMap);
-        innerMap = editItemCompletionStatus(innerMap);
-        outerMap = editToDoListTitle(outerMap, innerMap);
-
-        innerMap = removeItem(innerMap);
-        outerMap = removeToDoList(outerMap, innerMap);
+    public static void main(String[] args) { //declare the main function/method
+        launch(); //utilize launch method to start the program
     }
 
-    public static class ItemDetails {
-        private String dueDate;
-        private String itemDescription;
-        private Integer completionFlag;
+    public static class ItemDetails { //declare a public static class to utilize as a struct and hold the information for each item
+        private String itemTitle; //declare a String variable, itemTitle, to hold the name or title of the item
+        private String itemDueDate; //declare a String variable, itemDueDate, to hold the due date of the item
+        private String itemDescription; //declare a String variable, itemDescription, to hold the description of the item
+        private Integer itemCompletionFlag; //declare an Integer variable, itemCompletionFlag, to hold whether the item has been marked in its respective checkbox or not
 
-        public String getDueDate() {
-            return dueDate;
-        }
+        public String getItemTitle() {
+            return itemTitle;
+        } //create getter for item's title
 
-        public void setDueDate(String newDueDate) {
-            this.dueDate = newDueDate;
-        }
+        public void setItemTitle(String newItemTitle) {
+            this.itemTitle = newItemTitle;
+        } //create setter for item's title
+
+        public String getItemDueDate() {
+            return itemDueDate;
+        } //create getter for item's due date
+
+        public void setItemDueDate(String newItemDueDate) {
+            this.itemDueDate = newItemDueDate;
+        } //create setter for item's due date
 
         public String getItemDescription() {
             return itemDescription;
-        }
+        } //create getter for item's description
 
         public void setItemDescription(String newItemDescription) {
             this.itemDescription = newItemDescription;
+        } //create setter for item's description
+
+        public Integer getItemCompletionFlag() {
+            return itemCompletionFlag;
+        } //create getter for item's completion flag / checkbox marker
+
+        public void setItemCompletionFlag(Integer itemCompletionFlag) { //create setter for item's completion flag / checkbox marker
+            this.itemCompletionFlag = itemCompletionFlag;
         }
-
-        public Integer getCompletionFlag() {
-            return completionFlag;
-        }
-
-        public void setCompletionFlag(Integer changedCompletionFlag) {
-            this.completionFlag = changedCompletionFlag;
-        }
     }
 
-    public static HashMap<String, HashMap<String, ItemDetails>> addToDoList(HashMap<String, HashMap<String, ItemDetails>> outerMap, HashMap<String, ItemDetails> innerMap) {
-        String toDoListTitle = ""; //will be initialized with the user's to-do list title input from the Screen Builder
-
-        outerMap.put(toDoListTitle, innerMap);
-        return outerMap;
-    }
-
-    public static HashMap<String, ItemDetails> addItem(HashMap<String, ItemDetails> innerMap) {
-        String itemTitle = ""; //will be initialized with the user's item title input from the Screen Builder
-
-        ItemDetails itemDetails = new ItemDetails();
-
-        String dueDate = ""; //will be initialized with the user's item due date input from the Screen Builder
-        itemDetails.setDueDate(dueDate);
-
-        String itemDescription = ""; //will be initialized with the user's item description from the Screen Builder
-        itemDetails.setItemDescription(itemDescription);
-
-        int completionFlag = 0; //will be initialized as unchecked or 0
-        itemDetails.setCompletionFlag(completionFlag);
-
-        innerMap.put(itemTitle, itemDetails);
-        return innerMap;
-    }
-
-    public static HashMap<String, HashMap<String, ItemDetails>> removeToDoList(HashMap<String, HashMap<String, ItemDetails>> outerMap, HashMap<String, ItemDetails> innerMap) {
-        String toDoListTitle = ""; //get the title of the to-do list that the user wants to remove
-
-        outerMap.remove(toDoListTitle, innerMap); //remove the specific to-do list from the to-do list / outer hashmap
-        return outerMap;
-    }
-
-    public static HashMap<String, ItemDetails> removeItem(HashMap<String, ItemDetails> innerMap) {
-        String itemTitle = ""; //get the title of the item that the user wants to remove
-
-        ItemDetails itemDetails = new ItemDetails();
-
-        innerMap.remove(itemTitle, itemDetails); //remove the specific item from the items list / inner hashmap
-        return innerMap;
-    }
-
-    public static HashMap<String, HashMap<String, ItemDetails>> editToDoListTitle(HashMap<String, HashMap<String, ItemDetails>> outerMap, HashMap<String, ItemDetails> innerMap) {
-        String toDoListTitle = ""; //get the title of the to-do list to navigate the hashmap
-
-        //transfer contents of current to-do list into a new to do list
-        //remove the old to-do list
-
-        return outerMap;
-    }
-
-    public static HashMap<String, ItemDetails> editItemDueDate(HashMap<String, ItemDetails> innerMap) {
-        String itemTitle = ""; //get the title of the item to navigate the hashmap
-
-        ItemDetails itemDetails = new ItemDetails();
-
-        String dueDate = ""; //update the due date with the user's new item due date input
-        itemDetails.setDueDate(dueDate);
-
-        innerMap.put(itemTitle, itemDetails);
-        return innerMap;
-    }
-
-    public static HashMap<String, ItemDetails> editItemDescription(HashMap<String, ItemDetails> innerMap) {
-        String itemTitle = ""; //get the title of the item to navigate the hashmap
-
-        ItemDetails itemDetails = new ItemDetails();
-
-        String itemDescription = ""; //update the item description with the user's new item description input
-        itemDetails.setItemDescription(itemDescription);
-
-        int completionFlag = 0;
-        itemDetails.setCompletionFlag(completionFlag);
-
-        innerMap.put(itemTitle, itemDetails);
-        return innerMap;
-    }
-
-    public static HashMap<String, ItemDetails> editItemCompletionStatus(HashMap<String, ItemDetails> innerMap) {
-        String itemTitle = ""; //get the title of the item to navigate the hashmap
-
-        ItemDetails itemDetails = new ItemDetails();
-
-        int completionFlag = 1; //update the completion flag with the current status of the item's checkbox
-        itemDetails.setCompletionFlag(completionFlag);
-
-        innerMap.put(itemTitle, itemDetails);
-        return innerMap;
-    }
-
-    @Override
+    @Override //incorporate override statement
+    //declare the start function/method with throws Exception in case there was an exception not considered in creating and launching the program
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
 
+        //declare a Parent variable, root, to hold the components of the fxml file
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("welcomeScreen.fxml")));
+
+        //declare a Scene variable, scene, to display the scene created via the fxml file
         Scene scene = new Scene(root);
 
-        stage.setScene(scene);
-        stage.setTitle("To-Do App");
-        stage.show();
+        stage.setScene(scene); //create the program window / stage for the scene
+        stage.setTitle("To-Do App"); //create the program window / stage title, To-Do App
+        stage.show(); //show the program window / stage
     }
 }
--- src/main/java/ucf/assignments/ApplicationController.java
+++ src/main/java/ucf/assignments/ApplicationController.java
@@ -1,17 +1,168 @@
 package ucf.assignments;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Sabrina Lopez
+ */
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import java.util.HashMap;
+import ucf.assignments.Application.*;
 
 public class ApplicationController extends javafx.application.Application {
+
+    HashMap<String, HashMap<String, Application.ItemDetails>> outerMap = new HashMap<>();
+    HashMap<String, Application.ItemDetails> innerMap = new HashMap<>();
+
+    public static HashMap<String, HashMap<String, ItemDetails>> addToDoList(HashMap<String, HashMap<String, ItemDetails>> outerMap, HashMap<String, ItemDetails> innerMap) {
+        String toDoListTitle = ""; //will be initialized with the user's to-do list title input from the Screen Builder
+
+        outerMap.put(toDoListTitle, innerMap);
+        return outerMap;
+    }
+
+    public static HashMap<String, ItemDetails> addItem(HashMap<String, ItemDetails> innerMap) {
+        String itemTitle = ""; //will be initialized with the user's item title input from the Screen Builder
+
+        ItemDetails itemDetails = new ItemDetails();
+
+        String dueDate = ""; //will be initialized with the user's item due date input from the Screen Builder
+        itemDetails.setItemDueDate(dueDate);
+
+        String itemDescription = ""; //will be initialized with the user's item description from the Screen Builder
+        itemDetails.setItemDescription(itemDescription);
+
+        int completionFlag = 0; //will be initialized as unchecked or 0
+        itemDetails.setItemCompletionFlag(completionFlag);
+
+        innerMap.put(itemTitle, itemDetails);
+        return innerMap;
+    }
+
+    public static HashMap<String, HashMap<String, ItemDetails>> removeToDoList(HashMap<String, HashMap<String, ItemDetails>> outerMap, HashMap<String, ItemDetails> innerMap) {
+        String toDoListTitle = ""; //get the title of the to-do list that the user wants to remove
+
+        outerMap.remove(toDoListTitle, innerMap); //remove the specific to-do list from the to-do list / outer hashmap
+        return outerMap;
+    }
+
+    public static HashMap<String, ItemDetails> removeItem(HashMap<String, ItemDetails> innerMap) {
+        String itemTitle = ""; //get the title of the item that the user wants to remove
+
+        ItemDetails itemDetails = new ItemDetails();
+
+        innerMap.remove(itemTitle, itemDetails); //remove the specific item from the items list / inner hashmap
+        return innerMap;
+    }
+
+    public static HashMap<String, HashMap<String, ItemDetails>> editToDoListTitle(HashMap<String, HashMap<String, ItemDetails>> outerMap, HashMap<String, ItemDetails> innerMap) {
+        String toDoListTitle = ""; //get the title of the to-do list to navigate the hashmap
+
+        //transfer contents of current to-do list into a new to do list
+        //remove the old to-do list
+
+        return outerMap;
+    }
+
+    public static HashMap<String, ItemDetails> editItemDueDate(HashMap<String, ItemDetails> innerMap) {
+        String itemTitle = ""; //get the title of the item to navigate the hashmap
+
+        ItemDetails itemDetails = new ItemDetails();
+
+        String dueDate = ""; //update the due date with the user's new item due date input
+        itemDetails.setItemDueDate(dueDate);
+
+        innerMap.put(itemTitle, itemDetails);
+        return innerMap;
+    }
+
+    public static HashMap<String, ItemDetails> editItemDescription(HashMap<String, ItemDetails> innerMap) {
+        String itemTitle = ""; //get the title of the item to navigate the hashmap
+
+        ItemDetails itemDetails = new ItemDetails();
+
+        String itemDescription = ""; //update the item description with the user's new item description input
+        itemDetails.setItemDescription(itemDescription);
+
+        int completionFlag = 0;
+        itemDetails.setItemCompletionFlag(completionFlag);
+
+        innerMap.put(itemTitle, itemDetails);
+        return innerMap;
+    }
+
+    public static HashMap<String, ItemDetails> editItemCompletionStatus(HashMap<String, ItemDetails> innerMap) {
+        String itemTitle = ""; //get the title of the item to navigate the hashmap
+
+        ItemDetails itemDetails = new ItemDetails();
+
+        int completionFlag = 1; //update the completion flag with the current status of the item's checkbox
+        itemDetails.setItemCompletionFlag(completionFlag);
+
+        innerMap.put(itemTitle, itemDetails);
+        return innerMap;
+    }
+
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
 
-        welcomeText.setText("Rawr");
+
+    @FXML
+    protected void onDeleteListClick() {
+
+        welcomeText.setText("Remove List");
+    }
+
+    @FXML
+    protected void onSaveListsClick() {
+
+        welcomeText.setText("Save List");
+    }
+
+    @FXML
+    protected void onLoadListsClick() {
+
+        welcomeText.setText("Load List");
+    }
+
+    @FXML
+    protected void onBackToHomeClick() {
+
+        welcomeText.setText("Back to Home");
+    }
+
+    @FXML
+    protected void onBackToListClick() {
+
+        welcomeText.setText("Back to List");
+    }
+
+    @FXML
+    protected void onMakeListClick() {
+
+        welcomeText.setText("Back to List");
+    }
+
+    @FXML
+    protected void onMakeItemClick() {
+
+        welcomeText.setText("Back to List");
+    }
+
+    @FXML
+    protected void onSubmitListClick() {
+
+        welcomeText.setText("Submitted List");
+    }
+
+    @FXML
+    protected void onSubmitItemClick() {
+
+        welcomeText.setText("Submitted Item");
     }
 
     @Override
